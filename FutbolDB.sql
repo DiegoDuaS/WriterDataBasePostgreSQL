8c9323e42d1cb6ce2b3cfbc4655a2241aab14bb0
xtsebas <thiagohugo2014@gmail.com>
2024-02-17 19:11:24 -0600

    fix: bug in the python script feat: sql where the queries for the tables are

---
+++ FutbolDB.sql
@@ -0,0 +1,129 @@
+CREATE TABLE leagues (
+    leagueID INT,
+    name VARCHAR(20),
+    understatNotation VARCHAR(20),
+	PRIMARY KEY (leagueID)
+);
+
+CREATE TABLE players (
+    playerID INT,
+    name VARCHAR(150),
+	PRIMARY KEY (playerID)
+);
+
+
+CREATE TABLE teams (
+    teamID INT PRIMARY KEY,
+    name VARCHAR(25)
+);
+
+CREATE TABLE games (
+    gameID INT,
+    leagueID INT,
+    season INT,
+    date varchar(150),
+    homeTeamID INT,
+    awayTeamID INT,
+    homeGoals INT,
+    awayGoals INT,
+    homeProbability FLOAT,
+    drawProbability FLOAT,
+    awayProbability FLOAT,
+    homeGoalsHalfTime INT,
+    awayGoalsHalfTime INT,
+    B365H FLOAT,
+    B365D FLOAT,
+    B365A FLOAT,
+    BWH FLOAT,
+    BWD FLOAT,
+    BWA FLOAT,
+    IWH FLOAT,
+    IWD FLOAT,
+    IWA FLOAT,
+    PSH FLOAT,
+    PSD FLOAT,
+    PSA FLOAT,
+    WHH FLOAT,
+    WHD FLOAT,
+    WHA FLOAT,
+    VCH FLOAT,
+    VCD FLOAT,
+    VCA FLOAT,
+    PSCH FLOAT,
+    PSCD FLOAT,
+    PSCA FLOAT,
+	PRIMARY KEY (gameID),
+    FOREIGN KEY (leagueID) REFERENCES leagues(leagueID),
+    FOREIGN KEY (homeTeamID) REFERENCES teams(teamID),
+    FOREIGN KEY (awayTeamID) REFERENCES teams(teamID)
+);
+
+
+CREATE TABLE appearances (
+    gameID INT,
+    playerID INT,
+    goals INT,
+    ownGoals INT,
+    shots INT,
+    xGoals FLOAT,
+    xGoalsChain FLOAT,
+    xGoalsBuildup FLOAT,
+    assists INT,
+    keyPasses INT,
+    xAssists FLOAT,
+    position VARCHAR(4),
+    positionOrder INT,
+    yellowCard INT,
+    redCard INT,
+    time INT,
+    substituteIn INT,
+    substituteOut INT,
+    leagueID INT,
+    PRIMARY KEY (gameID, playerID),
+    FOREIGN KEY (gameID) REFERENCES games(gameID),
+    FOREIGN KEY (playerID) REFERENCES players(playerID),
+    FOREIGN KEY (leagueID) REFERENCES leagues(leagueID)
+);
+
+
+CREATE TABLE shots (
+    gameID INT,
+    shooterID INT,
+    assisterID INT NULL,
+    minute INT,
+    situation VARCHAR(15),
+    lastAction VARCHAR(15),
+    shotType VARCHAR(15),
+    shotResult VARCHAR(15),
+    xGoal FLOAT,
+    positionX FLOAT,
+    positionY FLOAT,
+    PRIMARY KEY (gameID, shooterID, assisterID),
+    FOREIGN KEY (gameID) REFERENCES games(gameID),
+    FOREIGN KEY (shooterID) REFERENCES players(playerID),
+    FOREIGN KEY (assisterID) REFERENCES players(playerID)
+);
+
+CREATE TABLE teamstats (
+    gameID INT,
+    teamID INT,
+    season INT,
+    date varchar(150),
+    location VARCHAR(1),
+    goals INT,
+    xGoals FLOAT,
+    shots INT,
+    shotsOnTarget INT,
+    deep INT,
+    ppda FLOAT,
+    fouls INT,
+    corners INT,
+    yellowCards INT,
+    redCards INT,
+    result VARCHAR(1),
+    PRIMARY KEY (gameID, teamID),
+    FOREIGN KEY (gameID) REFERENCES games(gameID),
+    FOREIGN KEY (teamID) REFERENCES teams(teamID)
+);
+
+
--- futbolreader.py
+++ futbolreader.py
@@ -9,7 +9,9 @@ def testear_conexion():
             dbname="FutbolStats_Proy1",
             user="postgres",
             password="",
-            host="127.0.0.1"
+            host="127.0.0.1",
+            port=5432
+
         )
         print("Se conecto a la base de datos.")
         conn.close()
@@ -18,40 +20,79 @@ def testear_conexion():
 testear_conexion()
 
 
+def countColumns(archivo_csv):
+    try:
+        with open(archivo_csv, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            
+            # Obtener la primera fila del archivo CSV que contiene el encabezado
+            header = next(csvreader)
+
+            # Contar el número de columnas en el encabezado
+            num_columnas = len(header)
+            
+            return num_columnas
+
+    except Exception as e:
+        print(f"Error al contar columnas: {e}")
+        return 0 
+
+
 # Conexión a la base de datos 
 conn = psycopg2.connect(
     dbname="FutbolStats_Proy1",
     user="postgres",
-    password="contraseña",
-    host="127.0.0.1"
+    password="",
+    host="127.0.0.1",
+    port=5432
 )
 
 cur = conn.cursor()
-#archivo_csv = "ruta/al/archivo.csv"
-#tabla_db = "nombre_tabla"
-
 
 # Función para escribir los datos de los archivos a la base de datos 
 def writer(tabla_db, archivo_csv):
 
-    sql_insert = sql.SQL("INSERT INTO {} VALUES (%s, %s, %s, ...)").format(sql.Identifier(tabla_db))
+    try:
+        num_columns = countColumns(archivo_csv)
+        # Construir la sentencia SQL con marcadores de posición
+        sql_insert = sql.SQL('INSERT INTO {} VALUES ({})').format(
+            sql.Identifier(tabla_db),
+            sql.SQL(',').join(sql.Placeholder() * num_columns)  # Ajustar según la cantidad de columnas en tu tabla
+        )
 
-    with open(archivo_csv, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        next(csvreader)
-        for row in csvreader:
-            # Insertar fila en la base de datos
-            cur.execute(sql_insert, row)
+        with open(archivo_csv, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            next(csvreader)
+            for row in csvreader:
+                # Convertir valores numéricos de cadena a sus tipos correspondientes
+                row = [int(val) if val.isdigit() and val.lower() != 'na' else
+                    float(val) if '.' in val and val.lower() != 'na' else
+                    None if val.lower() == 'na' else
+                    val for val in row]
+                
+                # Insertar fila en la base de datos
+                cur.execute(sql_insert, row)
 
-    # Confirmar los cambios y cerrar la conexión
-    conn.commit()
-    cur.close()
-    conn.close()
+        # Confirmar los cambios y cerrar la conexión
+        conn.commit()
+        print(f'Datos insertados en la tabla {tabla_db}.')
 
-# writer("appearences","C:\Users\diego\OneDrive\Escritorio\2024\SEMESTRE V\BD\appearances.csv")
-# writer("games","C:\Users\diego\OneDrive\Escritorio\2024\SEMESTRE V\BD\appearances.csv")
-# writer("leagues","C:\Users\diego\OneDrive\Escritorio\2024\SEMESTRE V\BD\appearances.csv")
-# writer("players","C:\Users\diego\OneDrive\Escritorio\2024\SEMESTRE V\BD\appearances.csv")
-# writer("shots","C:\Users\diego\OneDrive\Escritorio\2024\SEMESTRE V\BD\appearances.csv")
-# writer("teams","C:\Users\diego\OneDrive\Escritorio\2024\SEMESTRE V\BD\appearances.csv")
-# writer("teamstats","C:\Users\diego\OneDrive\Escritorio\2024\SEMESTRE V\BD\appearances.csv")
+
+    except Exception as e:
+        # Manejar cualquier error y cerrar la conexión en caso de problemas
+        print(f"Error: {e}")
+        conn.rollback()
+
+    finally:
+        cur.close()
+        conn.close()
+
+
+
+#writer("leagues","tu_direccion")
+#writer("players","tu_direccion")
+#writer("teams","tu_direccion")
+#writer("games","tu_direccion")
+#writer("appearances","tu_direccion")
+#writer("shots","tu_direccion")
+#writer("teamstats","tu_direccion")
